a49cd23242fc5c353aa2915f62ce1ab7e243485b
SeyedHosseinTafakh <colonel.hossein@gmail.com>
2018-04-23 04:13:11 +0430

    hamed train it

---
+++ ssd300.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 22 00:05:07 2018
+
+@author: hossein
+"""
+#import dependensices
+
+from keras.optimizers import Adam, SGD
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TerminateOnNaN, CSVLogger
+from keras import backend as K
+from keras.models import load_model
+from math import ceil
+import numpy as np
+from matplotlib import pyplot as plt
+
+from models.keras_ssd300 import ssd_300
+from keras_loss_function.keras_ssd_loss import SSDLoss
+from keras_layers.keras_layer_AnchorBoxes import AnchorBoxes
+from keras_layers.keras_layer_DecodeDetections import DecodeDetections
+from keras_layers.keras_layer_DecodeDetections2 import DecodeDetections2
+from keras_layers.keras_layer_L2Normalization import L2Normalization
+
+from ssd_encoder_decoder.ssd_input_encoder import SSDInputEncoder
+from ssd_encoder_decoder.ssd_output_decoder import decode_detections, decode_detections_fast
+
+from data_generator.object_detection_2d_data_generator import DataGenerator
+from data_generator.object_detection_2d_geometric_ops import Resize
+from data_generator.object_detection_2d_photometric_ops import ConvertTo3Channels
+from data_generator.data_augmentation_chain_original_ssd import SSDDataAugmentation
+from data_generator.object_detection_2d_misc_utils import apply_inverse_transforms
+
+
+#image height and image width
+img_height = 300
+img_width = 300
+img_channels = 3
+mean_color = [123,117,107]
+
+
+swap_channels =[2,1,0]
+
+
+n_classes = 20#Number of positive  classes , e.g. 20 
+scales_pascal = [0.1, 0.2, 0.37, 0.54, 0.71, 0.88, 1.05] # The anchor box scaling factors used in the original SSD300 for the Pascal VOC datasets
+scales_coco = [0.07, 0.15, 0.33, 0.51, 0.69, 0.87, 1.05] # The anchor box scaling factors used in the original SSD300 for the MS COCO datasets
+
+scales = scales_pascal
+aspect_ratios = [[1.0, 2.0, 0.5],
+                 [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
+                 [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
+                 [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
+                 [1.0, 2.0, 0.5],
+                 [1.0, 2.0, 0.5]]
+
+two_boxes_for_ar1 = True
+steps = [8,16,32,64,100,300]
+
+offsets = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+
+clip_boxes = False
+
+variances = [0.1, 0.1, 0.2 , 0.2]
+
+normalize_coords = True
+
+# 1 : Build the Keras model .
+K.clear_session()
+
+
+model = ssd_300 (image_size=(img_height, img_width, img_channels),
+                n_classes=n_classes,
+                mode='training',
+                l2_regularization=0.0005,
+                scales=scales,
+                aspect_ratios_per_layer=aspect_ratios,
+                two_boxes_for_ar1=two_boxes_for_ar1,
+                steps=steps,
+                offsets=offsets,
+                clip_boxes=clip_boxes,
+                variances=variances,
+                normalize_coords=normalize_coords,
+                subtract_mean=mean_color,
+                swap_channels=swap_channels)
+# 2: Load some weights into the model.
+
+#TODO: set the path to the weights you want to load.
+weight_path = 'weights/ssd300_weights.h5'
+
+model.load_weights(weight_path , by_name=True)
+
+
+
+
+
+
+
+
+
+
+
